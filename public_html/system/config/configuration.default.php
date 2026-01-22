ba22d3ca33e86ec7f123005f2e5ed519a700dbc4
Davide Iafrate <Tuxliri@users.noreply.github.com>
2026-01-22 14:12:55 +0800

    Update ASSETS_STORE_URL to new repository

--- public_html/system/config/configuration.default.php
+++ public_html/system/config/configuration.default.php
@@ -7,7 +7,7 @@ $WEBAPI_VERSION = '1.0';
 $CACHE_SYSTEM = 'apcu';
 
 /* URL to a \compose\ assets store */
-$ASSETS_STORE_URL = 'https://raw.githubusercontent.com/afdaniele/compose-assets-store/';
+$ASSETS_STORE_URL = 'https://raw.githubusercontent.com/duckietown/compose-assets-store/';
 
 /* Branch of the \compose\ assets store defined in $ASSETS_STORE_URL */
 $ASSETS_STORE_VERSION = 'v3';
